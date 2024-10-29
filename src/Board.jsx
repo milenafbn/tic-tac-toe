d845a48c2416d2859664d03e587b051fa739f2fb
Milena Neves <80131766+milenafbn@users.noreply.github.com>
2024-10-28 22:36:18 -0300

    feat: :sparkles: add restart functionality

--- src/Board.jsx
+++ src/Board.jsx
@@ -27,6 +27,11 @@ export default function Board() {
         setXIsNext(!xIsNext);
     }
 
+    function handleRestart() {
+        setSquares(Array(9).fill(null));
+        setXIsNext(true);
+    }
+
     const winner = calculateWinner(squares);
     let status;
     if (winner) {
@@ -39,22 +44,33 @@ export default function Board() {
     <>
         <div className="status">{status}</div>
 
-        <div className="board-row">
-            <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-            <Square value={squares[1]} onSquareClick={() => handleClick(1)}  />
-            <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+        <div className="board">
+          <div className="board-row">
+              <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+              <Square value={squares[1]} onSquareClick={() => handleClick(1)}  />
+              <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+          </div>
+          <div className="board-row">
+              <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+              <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+              <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+          </div>
+          <div className="board-row">
+              <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+              <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+              <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+          </div>
         </div>
-        <div className="board-row">
-            <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-            <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-            <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-        </div>
-        <div className="board-row">
-            <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-            <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-            <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-        </div>
+
+        <Restart onStartClick={handleRestart} />
+
     </>
+    );
+}
+
+  function Restart({onStartClick}) {
+    return (
+      <button className="start" onClick={onStartClick}>restart</button>
     );
   }
 
